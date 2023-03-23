49cc2cfba22bdfdeefd60df2f020765ede7eef29
Allen Lewis <allenlewis32@gmail.com>
2023-03-23 14:54:02 +0530

    SortedSet

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,12 +5,15 @@
 
         static void Main(string[] args)
         {
-            var set = new HashSet<int>() { 1, 2, 3, 2};
-            set.Add(4);
-            set.Add(1);
-            foreach (var item in set)
+            var setInt = new SortedSet<int>() { 5, 1, 4, 2, 3, 2};
+            var setString = new SortedSet<string>() { "Maria", "Ioannes", "Baptista", "Maria" };
+            foreach ( var item in setInt )
             {
-                Console.WriteLine(item);
+                Console.WriteLine("int - " + item);
+            }
+            foreach( var item in setString )
+            {
+                Console.WriteLine("str - " + item);
             }
         }
     }
