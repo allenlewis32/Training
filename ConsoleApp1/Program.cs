225278a5d15c9813f9861fbb7851754be9f7fa52
Allen Lewis <allenlewis32@gmail.com>
2023-03-23 12:32:47 +0530

    Exception Handling

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,26 +1,21 @@
 ﻿class Program
 {
-    string name;
-    int id;
-    string dept;
-    float grade;
-    
-    Program(string name, int id, string dept, float grade)
-    {
-        this.name = name;
-        this.id = id;
-        this.dept = dept;
-        this.grade = grade;
-    }
     static void Main(string[] args)
     {
-        Program program = new Program("Allen Lewis", 2, "IT", 9.2f);
-        Program program2 = new Program("Maria Ioannes Baptista", 7, "CS", 9.7f);
-        program.PrintDetails();
-        program2.PrintDetails();
-    }
-    void PrintDetails()
-    {
-        Console.WriteLine($"Name: {name}\nID: {id}\nDept: {dept}\nGrade: {grade:0.00}\n");
+        int a = 10;
+        int b = 0;
+        try
+        {
+            int c = a / b;
+            Console.WriteLine(c);
+        }
+        catch(DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Exception Caught: {ex.Message}");
+        }
     }
 }
