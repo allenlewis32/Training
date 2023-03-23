bea3074f6a1e5e61b483bd3ae495740f7d84b42b
Allen Lewis <allenlewis32@gmail.com>
2023-03-23 14:48:15 +0530

    HashSet

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,13 +5,10 @@
 
         static void Main(string[] args)
         {
-            var list = new List<string>() { "s1", "s2"};
-            list.Add("s3");
-            list.Add("s4");
-            list.Add("s5");
-            list.Remove("s3");
-            list.Add("s5");
-            foreach (var item in list)
+            var set = new HashSet<int>() { 1, 2, 3, 2};
+            set.Add(4);
+            set.Add(1);
+            foreach (var item in set)
             {
                 Console.WriteLine(item);
             }
