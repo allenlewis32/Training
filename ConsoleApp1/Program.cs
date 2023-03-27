cad0c34f7c25d1c6299ef2045156e17438c38e60
Allen Lewis <allenlewis32@gmail.com>
2023-03-27 12:16:11 +0530

    Override

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Marks marks = new(475, 5);
-            Student student = new("AL", 23, 1, 2, marks);
-            Console.WriteLine("Percentage: " + student.GetPercent() + "%");
+            C1 c1 = new C1();c1.Foo();
+            C2 c2 = new C2();c2.Foo();
+        }
+    }
+    class C1
+    {
+        public virtual void Foo()
+        {
+            Console.WriteLine("C1");
+        }
+    }
+    class C2: C1
+    {
+        public sealed override void Foo()
+        {
+            Console.WriteLine("C2");
         }
     }
 }
