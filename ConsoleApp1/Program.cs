9551c38e26637176705a3c98bedafcd42887c90f
Allen Lewis <allenlewis32@gmail.com>
2023-03-27 10:41:28 +0530

    Inheritance

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,32 +4,13 @@
     {
         static void Main(string[] args)
         {
-            SortedDictionary<string, string> sd = new SortedDictionary<string, string>();
-            sd.Add("d", "dog");
-            sd.Add("a", "apple");
-            sd.Add("c", "cat");
-            sd.Add("b", "ball");
-            sd.Add("3", "three");
-            sd.Add("1", "one");
-            sd.Add("2", "two");
-            sd.Remove("3");
-            foreach(var kv in sd.OrderBy(x => x.Value))
-            {
-                if (sd[kv.Key] == "one")
-                {
-                    sd.Remove(kv.Key);
-                }
-            }
-            foreach(var kv in sd.OrderBy(x => x.Value))
-            {
-                Console.WriteLine(kv);
-            }
-            Console.WriteLine();
-            foreach(var kv in sd)
-            {
-                Console.WriteLine(kv);
-            }
-            Console.WriteLine(sd.GetValueOrDefault("10", "ten"));
+            Volvo volvo = new();
+            volvo.start();
+            Console.WriteLine("Max speed: " + volvo.GetMaxSpeed());
+            volvo.stop();
+            volvo.ABS();
+            Car c = volvo;
+            c.ABS();
         }
     }
 }
