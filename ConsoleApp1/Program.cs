cab0f01039985d1fbf4c39f9de64ce1fb0adadab
Allen Lewis <allenlewis32@gmail.com>
2023-03-28 14:17:53 +0530

    Lambda Expressions - 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,17 +6,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var add = (int a, int b) => a + b;
-            var sub = (int a, int b) => a - b;
-            Console.WriteLine(add(5, 7));
-            Console.WriteLine(sub(5, 7));
-            var print = (string msg) => Console.WriteLine(msg);
-            print("Hello");
-            var stack = add + sub;
-            foreach(var d in stack.GetInvocationList())
+            List<Student> students = new List<Student>
             {
-                Console.WriteLine(d.DynamicInvoke(1, 2));
+                new Student() { id = 1, name = "Allen", age = 23, dept = "IT", score = 91 },
+                new Student() { id = 2, name = "Lewis", age = 27, dept = "CSE", score = 89 },
+                new Student() { id = 3, name = "Rheona", age = 14, dept = "PHY", score = 90 },
+                new Student() { id = 4, name = "Patricia", age = 17, dept = "ZOO", score = 92 }
+            };
+            foreach(var student in students)
+            {
+                Console.WriteLine($"ID: {student.id}, Name: {student.name}, Age: {student.age}, Dept: {student.dept}, Score: {student.score}");
+            }
+            Console.WriteLine();
+            List<Student> newStudentList = students.FindAll(x => x.age >= 18);
+            foreach (var student in newStudentList)
+            {
+                Console.WriteLine($"ID: {student.id}, Name: {student.name}, Age: {student.age}, Dept: {student.dept}, Score: {student.score}");
             }
         }
+    }
+    class Student
+    {
+        public int id;
+        public string name;
+        public int age;
+        public string dept;
+        public int score;
     }
 }
