6ec09fa25d7a3b30de615f69ab4c68d8ad277edd
Allen Lewis <allenlewis32@gmail.com>
2023-03-28 10:30:46 +0530

    Stacking Delegates

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,32 +3,25 @@
     class Program
     {
         delegate void z();
-        delegate int calc(int x, int y);
+        delegate void calc(int x, int y);
         static void Main(string[] args)
         {
             z z = cl.f;
-            z(); 
+            z();
             calc calcDelegate = Add;
-            Console.WriteLine(calcDelegate(5, 7));
+            calcDelegate(5, 7);
             calcDelegate += Mul;
-            Console.WriteLine(calcDelegate.Invoke(12, 13));
+            calcDelegate.Invoke(12, 13);
             calcDelegate -= Mul;
-            Console.WriteLine(calcDelegate.Invoke(12, 13));
-
-            calcDelegate = Add;
-            calcDelegate += Mul;
-            foreach(calc calc in calcDelegate.GetInvocationList())
-            {
-                Console.WriteLine(calc.Invoke(5, 7));
-            }
+            calcDelegate.Invoke(12, 13);
         }
-        static int Add(int x, int y)
+        static void Add(int x, int y)
         {
-            return x + y;
+            Console.WriteLine(x + y);
         }
-        static int Mul(int x, int y)
+        static void Mul(int x, int y)
         {
-            return x * y;
+            Console.WriteLine(x * y);
         }
     }
     public class cl
