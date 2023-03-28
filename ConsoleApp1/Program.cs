5481953a4ba3ddac2bbb94b09365b6f47f55a8fa
Allen Lewis <allenlewis32@gmail.com>
2023-03-28 10:06:32 +0530

    Delegates

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,41 @@
-﻿using System.Text;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     class Program
     {
+        delegate void z();
+        delegate int calc(int x, int y);
         static void Main(string[] args)
         {
-            const string path = "c:\\Users\\HP\\Desktop\\test";
-            DirectoryInfo info = new DirectoryInfo(path);
-            info.Create();
+            z z = cl.f;
+            z(); 
+            calc calcDelegate = Add;
+            Console.WriteLine(calcDelegate(5, 7));
+            calcDelegate += Mul;
+            Console.WriteLine(calcDelegate.Invoke(12, 13));
+            calcDelegate -= Mul;
+            Console.WriteLine(calcDelegate.Invoke(12, 13));
+
+            calcDelegate = Add;
+            calcDelegate += Mul;
+            foreach(calc calc in calcDelegate.GetInvocationList())
+            {
+                Console.WriteLine(calc.Invoke(5, 7));
+            }
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+    }
+    public class cl
+    {
+        static public void f()
+        {
+            Console.WriteLine("f");
         }
     }
 }
