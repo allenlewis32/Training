a70f7bc0a3989c781d5e3379f67036ba73695cbe
Allen Lewis <allenlewis32@gmail.com>
2023-03-28 11:04:35 +0530

    Anonymous Functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,41 +1,17 @@
-﻿namespace ConsoleApp1
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
 {
     class Program
     {
-        delegate void z();
-        delegate int calc(int x, int y);
+        delegate int Calc(int a, int b);
+        delegate void Print(string msg);
         static void Main(string[] args)
         {
-            z z = cl.f;
-            z(); 
-            calc calcDelegate = Add;
-            Console.WriteLine(calcDelegate(5, 7));
-            calcDelegate += Mul;
-            Console.WriteLine(calcDelegate.Invoke(12, 13));
-            calcDelegate -= Mul;
-            Console.WriteLine(calcDelegate.Invoke(12, 13));
-
-            calcDelegate = Add;
-            calcDelegate += Mul;
-            foreach(calc calc in calcDelegate.GetInvocationList())
-            {
-                Console.WriteLine(calc.Invoke(5, 7));
-            }
-        }
-        static int Add(int x, int y)
-        {
-            return x + y;
-        }
-        static int Mul(int x, int y)
-        {
-            return x * y;
-        }
-    }
-    public class cl
-    {
-        static public void f()
-        {
-            Console.WriteLine("f");
+            Calc add = delegate (int a, int b) { return a + b; };
+            Console.WriteLine(add(5, 7));
+            Print print = delegate(string msg) { Console.WriteLine(msg); };
+            print("Hello");
         }
     }
 }
