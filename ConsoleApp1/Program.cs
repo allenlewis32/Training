a57478c2dafeccb79cb10ff21a5803ded7b3804b
Allen Lewis <allenlewis32@gmail.com>
2023-03-28 14:34:12 +0530

    Lambda Expression - 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,8 @@ namespace ConsoleApp1
         {
             List<Student> students = new List<Student>
             {
-                new Student() { id = 1, name = "Allen", age = 23, dept = "IT", score = 91 },
-                new Student() { id = 2, name = "Lewis", age = 27, dept = "CSE", score = 89 },
+                new Student() { id = 1, name = "Allen", age = 27, dept = "IT", score = 91 },
+                new Student() { id = 2, name = "Lewis", age = 23, dept = "CSE", score = 89 },
                 new Student() { id = 3, name = "Rheona", age = 14, dept = "PHY", score = 90 },
                 new Student() { id = 4, name = "Patricia", age = 17, dept = "ZOO", score = 92 }
             };
@@ -18,7 +18,9 @@ namespace ConsoleApp1
                 Console.WriteLine($"ID: {student.id}, Name: {student.name}, Age: {student.age}, Dept: {student.dept}, Score: {student.score}");
             }
             Console.WriteLine();
+            students.ForEach(x => x.id++);
             List<Student> newStudentList = students.FindAll(x => x.age >= 18);
+            newStudentList.Sort((s1, s2) => s1.age - s2.age);
             foreach (var student in newStudentList)
             {
                 Console.WriteLine($"ID: {student.id}, Name: {student.name}, Age: {student.age}, Dept: {student.dept}, Score: {student.score}");
