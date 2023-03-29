443d4b2baf3a572341940e43fb67fe866c5a0d51
Allen Lewis <allenlewis32@gmail.com>
2023-03-29 09:57:41 +0530

    Predicates

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,20 +4,10 @@ namespace ConsoleApp1
 {
     class Program
     {
-        delegate void Print();
         static void Main(string[] args)
         {
-            Print print = HelloWorld;
-            print();
-            Action<string> p = (msg) => Console.WriteLine(msg);
-            p("Action");
-            Func<int, int> abs = (x) => x >= 0 ? x : -x;
-            Console.WriteLine(abs(5));
-            Console.WriteLine(abs(-7));
-        }
-        public static void HelloWorld()
-        {
-            Console.WriteLine("Hello, World!");
+            Func<int, int, bool> g = (a, b) => (a > b);
+            Predicate<int> p = (a) => (a > 5);
         }
     }
 }
